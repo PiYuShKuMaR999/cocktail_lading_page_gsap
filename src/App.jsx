ae206c315e75e3eccc91613031ece365b6881c75
Piyushkumar <piyush990841@gmail.com>
2025-07-07 02:39:49 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import gsap from 'gsap';
+import {ScrollTrigger, SplitText } from "gsap/all"
+gsap.registerPlugin(ScrollTrigger, SplitText);
+import Navbar from './components/Navbar.jsx';
+import Hero from './components/Hero.jsx';
+import Cocktails from './components/Cocktails.jsx';
+import About from './components/About.jsx';
+import Art from './components/Art.jsx';
+import Menu from './components/Menu.jsx';
+
+const App = () => {
+  return (
+   <main>
+    <Navbar />
+    <Hero />
+    <Cocktails />
+    <About />
+    <Art />
+    <Menu />
+   </main>
+  )
+}
+
+export default App
